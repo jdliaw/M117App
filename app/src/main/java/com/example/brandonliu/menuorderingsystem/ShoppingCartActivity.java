eb3229b0949fcec062ad2401307c5dd0a537fcda
Brandon Liu <brandonliu@s-128-97-104-148.resnet.ucla.edu>
2016-02-25 00:50:54 -0800

    added total cost + number formatting to shoppingcartactivity

--- app/src/main/java/com/example/brandonliu/menuorderingsystem/ShoppingCartActivity.java
+++ app/src/main/java/com/example/brandonliu/menuorderingsystem/ShoppingCartActivity.java
@@ -8,32 +8,54 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.text.DecimalFormat;
 
 public class ShoppingCartActivity extends AppCompatActivity {
 
  //   private ArrayList<String> shoppingCart = new ArrayList<String>();
+    private double total;
+    private double tax;
+    private final static double taxRate = .08; //will be gotten based off location
+    private static DecimalFormat dec = new DecimalFormat("#.00");
+    /*
+    Double number = Double.valueOf(text);
+
+    DecimalFormat dec = new DecimalFormat("#.00 EUR");
+    String credits = dec.format(number);
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
-        //ArrayList<String> cart; cart = getIntent().getStringArrayListExtra("paramName");Log.d("117", cart.get(0));
 
-        //parcelable
+
+
+        //get Array List of menu items
         ArrayList<MenuItem> parcelCart = getIntent().getParcelableArrayListExtra("paramName");
         Log.d("shoppingCartSize", String.valueOf(parcelCart.size()));
+
+
         //convert arrayList<MenuItem> into arrayList<String>
         ArrayList<String> shoppingCart = getCart(parcelCart);
 
-        Log.d("arraylist_string", shoppingCart.get(0));
+        //get total quantity
+        tax = getSubtotal(parcelCart) * taxRate;
+        total = tax + getSubtotal(parcelCart);
+        shoppingCart.add("Total: $" + dec.format(total));
 
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, shoppingCart);
 
         final ListView listView = (ListView)findViewById(R.id.shopping_cart);
         //sets adapter and creates the listview
         listView.setAdapter(itemsAdapter);
-
-
-
+    }
+    public static double getSubtotal(ArrayList<MenuItem> allItems) {
+        int len = allItems.size();
+        double tot = 0;
+        for(int i = 0; i < len; i++) {
+            tot += (allItems.get(i).getPrice() * allItems.get(i).getQuantity());
+        }
+        return tot;
     }
 
     public static ArrayList<String> getCart(ArrayList<MenuItem> allItems) {
@@ -42,9 +64,11 @@ public class ShoppingCartActivity extends AppCompatActivity {
         int len = allItems.size();
         for(int i = 0; i < len; i++) {
             //create string that gets added to the arraylist of strings
-            String temp = allItems.get(i).getName() + "      Price: $" + String.valueOf(allItems.get(i).getPrice()) +
-                    "      Quantity: " + allItems.get(i).getQuantity();
-
+            double price = allItems.get(i).getQuantity() * allItems.get(i).getPrice();
+            String temp = allItems.get(i).getPrice() + "         " +
+                    allItems.get(i).getName() + "          $" +
+                    dec.format(price);
+            Log.d("print tempCart", temp);
             tempCart.add(temp);
         }
         return tempCart;
